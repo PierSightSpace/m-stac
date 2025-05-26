b542a948766ee199e89655e4d71cbc7ad2d95fe2
niral2611 <niral.ajmera@piersight.space>
2025-05-26 18:06:34 +0530

    Added Docstrings

--- auth-service/utils.py
+++ auth-service/utils.py
@@ -52,50 +52,50 @@ def verify_password(password_to_be_checked, hashed_correct_password):
     return pwd_context.verify(password_to_be_checked, hashed_correct_password)
 
 
-def _generate_secret_key(length=32):
-    """
-    Generates a secret key for CSRF protection.
+# def _generate_secret_key(length=32):
+#     """
+#     Generates a secret key for CSRF protection.
 
-    Parameters:
-        length: The length of the secret key to generate.
+#     Parameters:
+#         length: The length of the secret key to generate.
 
-    Returns:
-        The generated secret key as a string.
-    """
-    alphabets = string.ascii_letters + string.digits
-    secret_key = ''.join(secrets.choice(alphabets) for _ in range(length))
-    return secret_key      
+#     Returns:
+#         The generated secret key as a string.
+#     """
+#     alphabets = string.ascii_letters + string.digits
+#     secret_key = ''.join(secrets.choice(alphabets) for _ in range(length))
+#     return secret_key      
 
 
-def _rotate_secret_key():
-    """
-    Rotates the CSRF secret key by generating a new one.
+# def _rotate_secret_key():
+#     """
+#     Rotates the CSRF secret key by generating a new one.
 
-    This function is thread-safe and updates the global secret key.
-    """
-    global SECRET_KEY_CSRF
-    with lock:
-        SECRET_KEY_CSRF = _generate_secret_key()
+#     This function is thread-safe and updates the global secret key.
+#     """
+#     global SECRET_KEY_CSRF
+#     with lock:
+#         SECRET_KEY_CSRF = _generate_secret_key()
 
     
-def schedule_key_rotation(rotation_interval_in_seconds=24*60*60):
-    """
-    Runs an infinite loop to rotate the CSRF secret key at a fixed interval.
+# def schedule_key_rotation(rotation_interval_in_seconds=24*60*60):
+#     """
+#     Runs an infinite loop to rotate the CSRF secret key at a fixed interval.
 
-    Parameters:
-        rotation_interval_in_seconds: The interval (in seconds) between key rotations.
-    """
-    while not stop_rotation.is_set():
-        _rotate_secret_key()
-        time.sleep(rotation_interval_in_seconds)
+#     Parameters:
+#         rotation_interval_in_seconds: The interval (in seconds) between key rotations.
+#     """
+#     while not stop_rotation.is_set():
+#         _rotate_secret_key()
+#         time.sleep(rotation_interval_in_seconds)
 
 
-def get_secret_key_csrf():
-    """
-    Retrieves the current CSRF secret key in a thread-safe manner.
+# def get_secret_key_csrf():
+#     """
+#     Retrieves the current CSRF secret key in a thread-safe manner.
 
-    Returns:
-        The current CSRF secret key.
-    """
-    with lock:
-        return SECRET_KEY_CSRF
+#     Returns:
+#         The current CSRF secret key.
+#     """
+#     with lock:
+#         return SECRET_KEY_CSRF
